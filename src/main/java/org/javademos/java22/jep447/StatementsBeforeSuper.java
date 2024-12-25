a6bf35075c678a72a5d8eb340d9fdcf66108a850
Alois Sečkár <ellrohir@seznam.cz>
2024-12-25 17:03:52 +0100

    feat: JEP 482 info

--- src/main/java/org/javademos/java22/jep447/StatementsBeforeSuper.java
+++ src/main/java/org/javademos/java22/jep447/StatementsBeforeSuper.java
@@ -4,11 +4,8 @@ import org.javademos.commons.IDemo;
 
 /// Demo for JDK 22 feature **Statements before super(...)** (JEP 447)
 ///
-/// JEP history:
-/// - JDK 22: [JEP 447 - Statements before super(...)](https://openjdk.org/jeps/447)
-///
-/// Further reading:
-/// - [Constructor Makeover in Java 22](https://blog.jetbrains.com/idea/2024/02/constructor-makeover-in-java-22/)
+/// Replaced with [JEP 482](https://openjdk.org/jeps/482)
+/// @see org.javademos.java23.jep482.FlexibleConstructorBodies
 ///
 /// @author alois.seckar@gmail.com
 
@@ -17,26 +14,6 @@ public class StatementsBeforeSuper implements IDemo {
     @Override
     public void demo() {
         info(447);
-        var x = new SuperChildOld();
-        var y = new SuperChildNew();
     }
 
-    private static class SuperParent {
-    }
-
-    /// Traditional way - super() must be first the first call.
-    private static class SuperChildOld extends SuperParent {
-        public SuperChildOld() {
-            // System.out.println("You can't do this before Java 22...");
-            super();
-        }
-    }
-
-    /// Statements before super in action - you can have Java code before calling parent constructor.
-    private static class SuperChildNew extends SuperParent {
-        public SuperChildNew() {
-            System.out.println("You can do this now in Java 22!");
-            super();
-        }
-    }
 }
--- src/main/java/org/javademos/java23/jep482/FlexibleConstructorBodies.java
+++ src/main/java/org/javademos/java23/jep482/FlexibleConstructorBodies.java
@@ -1,21 +1,57 @@
 package org.javademos.java23.jep482;
 
+import lombok.Getter;
 import org.javademos.commons.IDemo;
 
-/**
- * <p>
- *   Demo for JDK 23 feature <strong>Flexible Constructor Bodies (Second Preview)</strong> (JEP 482)
- * </p>
- * <p>
- *   Further reading: TBA
- * </p>
- *
- * @see <a href="https://openjdk.org/jeps/482">Flexible Constructor Bodies (Second Preview)</a>
- * @author alois.seckar@gmail.com
- */
+/// Demo for JDK 23 feature **Flexible Constructor Bodies (Second Preview)** (JEP 482)
+///
+/// JEP history:
+/// - JDK 23: [JEP 482 - Flexible Constructor Bodies (Second Preview)](https://openjdk.org/jeps/482)
+/// - JDK 22: [JEP 447 - Statements before super(...) (Preview)](https://openjdk.org/jeps/447)
+///
+/// - Will have new implementation in JDK 23 via [JEP 492](https://openjdk.org/jeps/492)
+///
+/// Further reading:
+/// - [Flexible Constructors in Java](https://medium.com/@nabeeltariqbhatti/flexible-constructors-in-java-e882a98de46c)
+/// - [Constructor Makeover in Java 22](https://blog.jetbrains.com/idea/2024/02/constructor-makeover-in-java-22/)
+///
+/// @author alois.seckar@gmail.com
+
 public class FlexibleConstructorBodies implements IDemo {
     @Override
     public void demo() {
         info(482);
+        var x = new FlexibleConstructorBodies.SuperChildOld();
+        var y = new FlexibleConstructorBodies.SuperChildNew();
+    }
+
+    private static class SuperParent {
+    }
+
+    /// Traditional way - super() must be first the first call.
+    private static class SuperChildOld extends FlexibleConstructorBodies.SuperParent {
+        public SuperChildOld() {
+            // System.out.println("You can't do this before Java 22...");
+            super();
+        }
+    }
+
+    /// Statements before super in action - you can have Java code before calling parent constructor.
+    /// Since JDK 23 it is also possible to initialize local variables prior to super(...) statement.
+    @Getter // just to avoid "unused variable" IDE warning
+    private static class SuperChildNew extends FlexibleConstructorBodies.SuperParent {
+
+        private final int value;
+
+        public SuperChildNew() {
+            System.out.println("You can do this now in Java 22 and on!");
+
+            // since Java 23 it is also possible to assign local variables before calling parent constructor
+            // however, you can't read them before you do
+            this.value = 1;
+            // System.out.println(this.value); // this would produce compiler error
+
+            super();
+        }
     }
 }
