f746dae8f6a4ee8a7700a752f63b24e50d987ca0
CodeSakshamY <166237273+CodeSakshamY@users.noreply.github.com>
2025-10-02 06:25:33 +0530

    Update Main.java

--- src/main/java/org/javademos/Main.java
+++ src/main/java/org/javademos/Main.java
@@ -1,62 +1,32 @@
-package org.javademos;
+package demos.jep519;
 
-import org.javademos.commons.IDemo;
-import org.javademos.init.*;
+import demos.IDemo;
 
-import java.util.ArrayList;
+public class CompactObjectHeaderDemo implements IDemo {
 
-/**
- * This application helps to show some new features from Java 11 to Java 21.
- * The 'demoPool' array consists of simple IDemo interface implementations.
- * Code logic is being implemented inside .demo() methods.
- * 
- * @see IDemo
- * 
- * You can try switching the compile/build settings back to Java 1.8 to see
- * that the most of the stuff used here cannot be compiled and run under old
- * versions of Java.
- * 
- * @author alois.seckar@gmail.com
- */
-public class Main {
-    
-    /**
-     * Simple main method.
-     * No complex logic - all available demo implementations are being put
-     * inside a list, and then being executed one by one.
-     * 
-     * @param args not supported (ignored) in this application
-     */
-    public static void main(String[] args) {
+    private static final int OBJECT_COUNT = 5_000_000;
 
-        // https://stackoverflow.com/a/19165338/3204544
-        System.out.println("JVM INFO");
-        System.out.println(System.getProperty("java.vm.name"));
-        System.out.println(System.getProperty("java.home"));
-        System.out.println(System.getProperty("java.vendor"));
-        System.out.println(System.getProperty("java.version"));
-        System.out.println(System.getProperty("java.specification.vendor"));
-        //
-        
-        var demoPool = new ArrayList<IDemo>();
-        
-        // you may find useful to comment out other JDKs except the one
-        // you are about to examine
-        // it may be confusing to search the actual output of particular
-        // demo class implementation you are currently interested in
+    @Override
+    public void demo() {
+        System.out.println("=== JEP 519: Compact Object Headers ===");
 
-        // older demos - not all JEPs covered yet
-        demoPool.addAll(Java11.getDemos());
-        demoPool.addAll(Java17.getDemos());
-        demoPool.addAll(Java21.getDemos());
-        demoPool.addAll(Java22.getDemos());
-        // newer demos - each JEP has separate file with info/examples
-        demoPool.addAll(Java23.getDemos());
-        demoPool.addAll(Java25.getDemos());
-        
-        // run method .demo() on each entry to see the output
-        demoPool.forEach(IDemo::demo);
-        
+        Runtime runtime = Runtime.getRuntime();
+
+        // Initial memory usage
+        long before = runtime.totalMemory() - runtime.freeMemory();
+
+        // Allocate objects
+        Object[] objects = new Object[OBJECT_COUNT];
+        for (int i = 0; i < OBJECT_COUNT; i++) {
+            objects[i] = new Object();
+        }
+
+        // Memory after allocation
+        long after = runtime.totalMemory() - runtime.freeMemory();
+
+        System.out.printf("Created %,d objects.%n", OBJECT_COUNT);
+        System.out.printf("Approximate memory used: %,.2f MB%n", (after - before) / (1024.0 * 1024.0));
+
+        System.out.println("Run this demo on Java 21 vs Java 25 to see the reduced memory footprint.");
     }
-    
 }
