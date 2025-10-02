240fe4ff4196f3c9125be6e5d1d0e1c02319f70c
Kishan_Singh <147229665+kishansinghifs1@users.noreply.github.com>
2025-10-03 03:36:21 +0530

    feat(jdk23): add demo for JEP 463 - Implicitly Declared Classes (#72)
    * Add demo for JEP 463 and update Java23.java
    
    * Update ImplicitlyDeclaredClassesDemo.java
    
    * Update ImplicitlyDeclaredClassesDemo.java

--- src/main/java/org/javademos/init/Java23.java
+++ src/main/java/org/javademos/init/Java23.java
@@ -2,6 +2,8 @@ package org.javademos.init;
 
 import org.javademos.commons.IDemo;
 import org.javademos.java23.jep455.PrimitiveTypesInPatterns;
+import org.javademos.java23.jep460.JEP460VectorApiDemo;
+import org.javademos.java23.jep463.ImplicitlyDeclaredClassesDemo;
 import org.javademos.java23.jep466.ClassFileAPI;
 import org.javademos.java23.jep467.MarkdownComments;
 import org.javademos.java23.jep469.VectorAPI;
@@ -13,7 +15,6 @@ import org.javademos.java23.jep477.ImplicitlyDeclaredClasses;
 import org.javademos.java23.jep480.StructuredConcurrency;
 import org.javademos.java23.jep481.ScopedValues;
 import org.javademos.java23.jep482.FlexibleConstructorBodies;
-import org.javademos.java23.jep460.JEP460VectorApiDemo;
 
 import java.util.ArrayList;
 
@@ -29,6 +30,8 @@ public class Java23 {
         java23DemoPool.add(new PrimitiveTypesInPatterns());
         // JEP 460
         java23DemoPool.add(new JEP460VectorApiDemo());
+        // JEP 463
+        java23DemoPool.add(new ImplicitlyDeclaredClassesDemo());
         // JEP 466
         java23DemoPool.add(new ClassFileAPI());
         // JEP 467
