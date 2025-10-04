dcf5ccba791bf2afa0dbda648d7607f777245499
Dhruv Agrawal <dhruvagrawal963@gmail.com>
2025-10-05 02:05:50 +0530

    feat(java24): Add demo for JEP 490 - ZGC Mode Removal (#115)
    Co-authored-by: Alois Sečkár <ellrohir@seznam.cz>

--- src/main/java/org/javademos/init/Java24.java
+++ src/main/java/org/javademos/init/Java24.java
@@ -5,8 +5,10 @@ import java.util.ArrayList;
 import org.javademos.commons.IDemo;
 import org.javademos.java24.jep478.KeyDerivationApiPreview;
 import org.javademos.java24.jep485.StreamGatherers;
-import org.javademos.java24.jep493.LinkingRunTimeImages493;
 import org.javademos.java24.jep486.DisableSecurityManager;
+import org.javademos.java24.jep490.ZgcNonGenerationalModeRemoval;
+import org.javademos.java24.jep493.LinkingRunTimeImages493;
+
 
 public class Java24 {
 
@@ -23,12 +25,12 @@ public class Java24 {
 
         // JEP 485
         java24DemoPool.add(new StreamGatherers());
-
-        // JEP 493
-        java24DemoPool.add(new LinkingRunTimeImages493());
-
         // JEP 486
         java24DemoPool.add(new DisableSecurityManager());
+        // JEP 490
+        java24DemoPool.add(new ZgcNonGenerationalModeRemoval());
+        // JEP 493
+        java24DemoPool.add(new LinkingRunTimeImages493());
 
         return java24DemoPool;
     }
