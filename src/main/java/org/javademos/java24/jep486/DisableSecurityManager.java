144812c8a54e8aa0005ddb7be55670f902d58741
shepherdking67 <shepherdking67@gmail.com>
2025-10-05 00:37:17 +0530

    Fix: add missing info(486) call and remove unnecessary toString override in JEP 486 demo

--- src/main/java/org/javademos/java24/jep486/DisableSecurityManager.java
+++ src/main/java/org/javademos/java24/jep486/DisableSecurityManager.java
@@ -13,21 +13,18 @@ public class DisableSecurityManager implements IDemo {
 
     @Override
     public void demo() {
+        // JEP reference info
+        info(486);
+
         // The Security Manager was deprecated for removal in earlier releases
         // and is now permanently disabled as of JDK 24.
         //
         // Any attempt to call System.setSecurityManager(...) results in an
         // UnsupportedOperationException.
-        
         try {
             System.setSecurityManager(new SecurityManager()); // this is removed
         } catch (UnsupportedOperationException e) {
             System.out.println("SecurityManager is permanently disabled: " + e);
         }
-    }
-
-    @Override
-    public String toString() {
-        return "JEP 486 - Permanently Disable the Security Manager";
     }
 }
