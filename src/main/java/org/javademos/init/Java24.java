a9d3b5f567b923f372c7164d41b29cb6d1561ef7
Sheph <shepherdking67@gmail.com>
2025-10-05 01:16:38 +0530

    feat(jdk24): add demo for JEP 486 - Permanently Disable the Security Manager (#113)
    * Add JEP 486 demo: Permanently Disable the Security Manager (initial changes)
    
    * Feat: register JEP 486 (Disable Security Manager) demo in Java24
    
    * Fix: add missing info(486) call and remove unnecessary toString override in JEP 486 demo
    
    * Style: updated JEP 486 demo to use Markdown documentation comments (///)
    
    ---------
    
    Co-authored-by: Alois Sečkár <ellrohir@seznam.cz>

--- src/main/java/org/javademos/init/Java24.java
+++ src/main/java/org/javademos/init/Java24.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import org.javademos.commons.IDemo;
 import org.javademos.java24.jep485.StreamGatherers;
 import org.javademos.java24.jep493.LinkingRunTimeImages493;
+import org.javademos.java24.jep486.DisableSecurityManager;
 
 public class Java24 {
 
@@ -15,12 +16,15 @@ public class Java24 {
         var java24DemoPool = new ArrayList<IDemo>();
 
         // feel free to comment out demos you are not interested in right now
-        
+
         // JEP 485
         java24DemoPool.add(new StreamGatherers());
-        
+
         // JEP 493
         java24DemoPool.add(new LinkingRunTimeImages493());
+
+        // JEP 486
+        java24DemoPool.add(new DisableSecurityManager());
 
         return java24DemoPool;
     }
---
+++ src/main/java/org/javademos/java24/jep486/DisableSecurityManager.java
@@ -0,0 +1,33 @@
+package org.javademos.java24.jep486;
+
+import org.javademos.commons.IDemo;
+
+/// Demo for JDK 24 feature **Permanently Disable the Security Manager** (JEP 486)
+///
+/// JEP history:
+/// - JDK 24: [JEP 486 - Permanently Disable the Security Manager](https://openjdk.org/jeps/486)
+///
+/// Further reading:
+/// - [JEP 411 (Deprecate the Security Manager)](https://openjdk.org/jeps/411)
+/// - [JEP 486 - Details](https://openjdk.org/jeps/486)
+///
+/// @author shepherdking67
+public class DisableSecurityManager implements IDemo {
+
+    @Override
+    public void demo() {
+        // JEP reference info
+        info(486);
+
+        // The Security Manager was deprecated for removal in earlier releases
+        // and is now permanently disabled as of JDK 24.
+        //
+        // Any attempt to call System.setSecurityManager(...) results in an
+        // UnsupportedOperationException.
+        try {
+            System.setSecurityManager(new SecurityManager()); // removed in JDK 24+
+        } catch (UnsupportedOperationException e) {
+            System.out.println("SecurityManager is permanently disabled: " + e);
+        }
+    }
+}
