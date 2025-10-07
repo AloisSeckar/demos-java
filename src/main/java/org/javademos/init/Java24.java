7a173cd6ad2a362f7380ed8bba16cc55d7146883
Dhruv Agrawal <dhruvagrawal963@gmail.com>
2025-10-07 12:17:52 +0530

    feat(java24): Add JEP 495d demo and link feature history  (#127)
    * feat(java24): Add demo for JEP 490 - ZGC Mode Removal
    
    * feat(java24): Add demo for JEP 491
    
    * feat(jep): Implement JEP 495
    
    * Changes
    
    ---------
    
    Co-authored-by: Alois Sečkár <ellrohir@seznam.cz>

--- src/main/java/org/javademos/init/Java24.java
+++ src/main/java/org/javademos/init/Java24.java
@@ -5,7 +5,6 @@ import java.util.ArrayList;
 import org.javademos.commons.IDemo;
 import org.javademos.java24.jep472.PrepareToRestrictJNI;
 import org.javademos.java24.jep478.KeyDerivationApiPreview;
-import org.javademos.java24.jep479.RemovedWindows32BitX86Demo;
 import org.javademos.java24.jep484.ClassFileAPIDemo;
 import org.javademos.java24.jep485.StreamGatherers;
 import org.javademos.java24.jep486.DisableSecurityManager;
@@ -13,9 +12,7 @@ import org.javademos.java24.jep489.VectorAPIDemo;
 import org.javademos.java24.jep490.ZgcNonGenerationalModeRemoval;
 import org.javademos.java24.jep491.SynchronizedNoPinning;
 import org.javademos.java24.jep493.LinkingRunTimeImages493;
-import org.javademos.java24.jep497.QuantumResistantModuleLatticeBasedDigitalSignature;
 import org.javademos.java24.jep498.MemoryAccessMethods;
-import org.javademos.java24.jep501.Deprecate32BitX86Demo;
 
 
 public class Java24 {
@@ -46,14 +43,8 @@ public class Java24 {
         java24DemoPool.add(new SynchronizedNoPinning());
         // JEP 493
         java24DemoPool.add(new LinkingRunTimeImages493());
-        // JEP 497
-        java24DemoPool.add(new QuantumResistantModuleLatticeBasedDigitalSignature());
         // JEP 498
         java24DemoPool.add(new MemoryAccessMethods());
-        // JEP 479
-        java24DemoPool.add(new RemovedWindows32BitX86Demo());
-        // JEP 501
-        java24DemoPool.add(new Deprecate32BitX86Demo());
 
         return java24DemoPool;
     }
