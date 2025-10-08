c0df657bd7601a280ceaa4de104cc5e3601d926d
Alois Sečkár <ellrohir@seznam.cz>
2025-10-08 19:26:04 +0200

    refactor: fix comment

--- src/main/java/org/javademos/java24/jep492/FlexibleConstructorBodies.java
+++ src/main/java/org/javademos/java24/jep492/FlexibleConstructorBodies.java
@@ -2,7 +2,7 @@ package org.javademos.java24.jep492;
 
 import org.javademos.commons.IDemo;
 
-/// Demo for JDK 24 feature **JEP 492 - Flexible Constructor Bodies (Third Preview)** (JEP 492)
+/// Demo for JDK 24 feature **JEP 492 - Flexible Constructor Bodies (Third Preview)**.
 ///
 /// Replaced with [JEP 513](https://openjdk.org/jeps/513)
 /// @see org.javademos.java25.jep513.FlexibleConstructorBodiesDemo
