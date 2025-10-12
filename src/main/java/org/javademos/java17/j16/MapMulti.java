1c8885cefba488b271772cd74b0a0a133e0d7b8d
Alois Sečkár <ellrohir@seznam.cz>
2025-10-12 10:03:49 +0200

    fix: repair broken lombok execution

--- src/main/java/org/javademos/java17/j16/MapMulti.java
+++ src/main/java/org/javademos/java17/j16/MapMulti.java
@@ -11,14 +11,6 @@ public class MapMulti {
     char first;
     boolean endsWithA;
 
-    // EXPLICIT "AllArgsConstructor"
-    // TODO delete when lombok fixed for JDK 23
-    public MapMulti(int length, char first, boolean endsWithA) {
-        this.length = length;
-        this.first = first;
-        this.endsWithA = endsWithA;
-    }
-
     @Override
     public String toString() {
         return "[" + length + " " + first + " " + endsWithA + "] ";
