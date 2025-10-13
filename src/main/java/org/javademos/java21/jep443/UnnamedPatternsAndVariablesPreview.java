4a9d667473644a504645abc658e5f00d72228d58
Dhruv Agrawal <dhruvagrawal963@gmail.com>
2025-10-13 19:56:03 +0530

    feat: Implement demo for JEP 456 and linking class for JEP 443 (#172)
    * feat: Implement demo for JEP 456 and linking class for JEP 443
    
    ---------
    
    Co-authored-by: Alois Sečkár <ellrohir@seznam.cz>

---
+++ src/main/java/org/javademos/java21/jep443/UnnamedPatternsAndVariablesPreview.java
@@ -0,0 +1,23 @@
+package org.javademos.java21.jep443;
+
+import org.javademos.commons.IDemo;
+
+/// Demo for JDK 21 feature **JEP 443 - Unnamed Patterns and Variables (preview)**.
+///
+/// **New version of this JEP exists: JEP 456 in JDK 22**
+/// This was the preview JEP for unnamed patterns and variables. The feature was
+/// finalized in JDK 22 by JEP 456.
+///
+/// This demo is a placeholder. For the full implementation, see the final JEP.
+///
+/// Superseded by [JEP 456](https://openjdk.org/jeps/456).
+/// @author [Krushit Babariya](https://github.com/Krushit-Babariya)
+/// @see org.javademos.java22.jep456.UnnamedPatternsAndVariablesDemo
+///
+public class UnnamedPatternsAndVariablesPreview implements IDemo {
+
+    @Override
+    public void demo() {
+        info(443);
+    }
+}
