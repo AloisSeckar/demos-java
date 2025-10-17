864a084239df039c9736079dbbc2dafd83913d51
MrChatterjee98 <biswarup244h@gmail.com>
2025-10-17 23:26:11 +0530

    feat(jep422):Linux/RISC-V Port

--- src/main/java/org/javademos/init/Java19.java
+++ src/main/java/org/javademos/init/Java19.java
@@ -3,6 +3,7 @@ package org.javademos.init;
 import java.util.ArrayList;
 
 import org.javademos.commons.IDemo;
+import org.javademos.java19.jep422.LinuxRiscVPort;
 import org.javademos.java19.jep424.ForeignFunctionMemoryDemo;
 import org.javademos.java19.jep426.VectorAPIDemo;
 import org.javademos.java19.jep427.PatternMatchingForSwitchThirdPreview;
@@ -16,7 +17,8 @@ public class Java19 {
         var java19DemoPool = new ArrayList<IDemo>();
 
         // feel free to comment out demos you are not interested in right now
-
+        // JEP 422
+        java19DemoPool.add(new LinuxRiscVPort());
         // JEP 424
         java19DemoPool.add(new ForeignFunctionMemoryDemo());
         // JEP 426
