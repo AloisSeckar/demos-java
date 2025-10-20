51e33e207aeb1791134dbc5576a9ad79c7c8636a
Harshit Kumar <harshitkumarhk1290@gmail.com>
2025-10-21 00:55:30 +0530

    feat: Add linking demo for JEP 450

---
+++ src/main/java/org/javademos/java24/jep450/CompactObjectHeadersExperimental.java
@@ -0,0 +1,17 @@
+package org.javademos.java24.jep450;
+
+import org.javademos.commons.IDemo;
+
+/// Demo for JDK 24 feature <strong>JEP 450 - Compact Object Headers (Experimental)</strong>.
+///
+/// This was an experimental version of the feature.
+/// The main demo is for the final implementation in JDK 25 (JEP 519).
+/// @see org.javademos.java25.jep519.CompactObjectHeaderDemo
+///
+/// @author harshitkumar7525
+public class CompactObjectHeadersExperimental implements IDemo {
+    @Override
+    public void demo() {
+        info(450);
+    }
+}
