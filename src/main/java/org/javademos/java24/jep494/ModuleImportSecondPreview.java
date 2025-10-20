a72614f0beff082dfe6b87ea2409db97d32504cd
Harshit Kumar <harshitkumarhk1290@gmail.com>
2025-10-20 19:01:57 +0530

    feat: Add linking demos for JEP 494 and JEP 476

---
+++ src/main/java/org/javademos/java24/jep494/ModuleImportSecondPreview.java
@@ -0,0 +1,17 @@
+package org.javademos.java24.jep494;
+
+import org.javademos.commons.IDemo;
+
+/// Demo for JDK 24 feature <strong>JEP 494 - Module Import Declarations (Second Preview)</strong>.
+///
+/// This was the second preview of this feature.
+/// The feature is finalized in JDK 25 (JEP 511).
+/// @see org.javademos.java25.jep511.ModuleImportDeclarationsDemo
+///
+/// @author harshitkumar7525
+public class ModuleImportSecondPreview implements IDemo {
+    @Override
+    public void demo() {
+        info(494);
+    }
+}
