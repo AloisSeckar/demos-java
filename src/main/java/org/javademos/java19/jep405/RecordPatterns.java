afb4b000daa49e74dae03eb4d3d61ff0c7fc010c
Ashutosh Singh <42292891+Rossville@users.noreply.github.com>
2025-10-22 19:42:59 +0530

    feat: add linking classes for JEP 405 and 432 (#239)
    ---------
    
    Co-authored-by: Alois Sečkár <ellrohir@seznam.cz>

---
+++ src/main/java/org/javademos/java19/jep405/RecordPatterns.java
@@ -0,0 +1,16 @@
+package org.javademos.java19.jep405;
+
+import org.javademos.commons.IDemo;
+
+/// Demo for JDK 19 feature <strong>JEP 405 - Record Patterns(Preview)</strong>.
+///
+/// This was an incubator JEP. The feature is finalized in JDK 21 by JEP 440.
+/// 
+/// @see org.javademos.java21.jep440.RecordPatternsDemo
+///
+/// @author @Rossville
+public class RecordPatterns implements IDemo {
+    public void demo() {
+        info(405);
+    }
+}
---
+++ src/main/java/org/javademos/java20/jep432/RecordPatterns.java
@@ -0,0 +1,17 @@
+package org.javademos.java20.jep432;
+
+import org.javademos.commons.IDemo;
+
+/// Demo for JDK 20 feature <strong>JEP 432 - Record Patterns(Second Preview)</strong>.
+///
+/// This was an incubator JEP. The feature is finalized in JDK 21 by JEP 440.
+/// 
+/// @see org.javademos.java21.jep440.RecordPatternsDemo
+///
+/// @author @Rossville
+public class RecordPatterns implements IDemo {
+    @Override
+    public void demo() {
+        info(432);
+    }
+}
