f6dd0ccb3bede701709ffae09e231fbf38d50f05
Rossville <ashutosh.singh.9510@gmail.com>
2025-10-22 20:23:13 +0530

    created and linked classes JEP 425 & JEP 436 with JEP 444

---
+++ src/main/java/org/javademos/java19/jep425/VirtualThreads.java
@@ -0,0 +1,15 @@
+package org.javademos.java19.jep425;
+import org.javademos.commons.IDemo;
+
+/// Demo for JDK 19 feature <strong>JEP 425 - Virtual Threads(Preview)</strong>.
+///
+/// This was an incubator JEP. The feature is finalized in JDK 21 by JEP 444.
+/// 
+/// @see org.javademos.java21.jep444.VirtualThreadsDemo
+///
+/// @author @Rossville
+public class VirtualThreads implements IDemo{
+    public void demo(){
+        info(425);
+    }
+}
---
+++ src/main/java/org/javademos/java20/jep436/VirtualThreads.java
@@ -0,0 +1,15 @@
+package org.javademos.java20.jep436;
+import org.javademos.commons.IDemo;
+
+/// Demo for JDK 20 feature <strong>JEP 436 - Virtual Threads(Second Preview)</strong>.
+///
+/// This was an incubator JEP. The feature is finalized in JDK 21 by JEP 444.
+/// 
+/// @see org.javademos.java21.jep444.VirtualThreadsDemo
+///
+/// @author @Rossville
+public class VirtualThreads implements IDemo{
+    public void demo(){
+        info(436);
+    }
+}
