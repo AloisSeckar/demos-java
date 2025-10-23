aedcc5dbf554acb2bdc3b23dcd9384866b543fa8
Nikesh-koila <kirthanbirwaz43@gmail.com>
2025-10-23 10:14:19 +0530

    feat(java16): Add demo for JEP 380 Unix Domain Sockets

--- src/main/java/org/javademos/init/Java16.java
+++ src/main/java/org/javademos/init/Java16.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 
 import org.javademos.commons.IDemo;
 import org.javademos.java16.jep338.VectorAPIDemo;
+import org.javademos.java16.jep380.UnixDomainSocketsDemo;
 import org.javademos.java16.jep389.ForeignLinkerDemo;
 import org.javademos.java16.jep393.ForeignMemoryAccessDemo;
 import org.javademos.java16.jep397.SealedClassesDemo;
@@ -20,6 +21,8 @@ public class Java16 {
 
         // JEP 338
         java16DemoPool.add(new VectorAPIDemo());
+        // JEP 380
+        java16DemoPool.add(new UnixDomainSocketsDemo());
         // JEP 389
         java16DemoPool.add(new ForeignLinkerDemo());
         // JEP 393
