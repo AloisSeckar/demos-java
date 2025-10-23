d6a8838e76007930a5592a072a881e4c75eb9021
Nikesh-koila <kirthanbirwaz43@gmail.com>
2025-10-23 10:23:57 +0530

    fix: Update class comment format to Markdown

--- src/main/java/org/javademos/java16/jep390/ValueBasedClassesDemo.java
+++ src/main/java/org/javademos/java16/jep390/ValueBasedClassesDemo.java
@@ -2,25 +2,23 @@ package org.javademos.java16.jep390;
 
 import org.javademos.commons.IDemo;
 
-/**
- * ## JEP 390: Warnings for Value-Based Classes
- *
- * ### History
- * Primitive wrapper classes (like Integer, Double) are now designated as
- * "value-based". This JEP adds new warnings when you try to use them in
- * ways that are not intended, like synchronizing on them.
- *
- * ### Links
- * - [Official JEP 390](https://openjdk.org/jeps/390)
- *
- * @since Java 16
- */
+/// ## JEP 390: Warnings for Value-Based Classes
+///
+/// ### History
+/// Primitive wrapper classes (like Integer, Double) are now designated as
+/// "value-based". This JEP adds new warnings when you try to use them in
+/// ways that are not intended, like synchronizing on them.
+///
+/// ### Links
+/// - [Official JEP 390](https://openjdk.org/jeps/390)
+///
+/// @since Java 16
 public class ValueBasedClassesDemo implements IDemo {
 
     @Override
     @SuppressWarnings("synchronization") // This is needed for mvn clean install to pass
     public void demo() {
-        // Call the info helper 
+        // Call the info helper
         info(390);
 
         // The Demo
@@ -31,7 +29,7 @@ public class ValueBasedClassesDemo implements IDemo {
         System.out.println("Attempting to synchronize on an instance of Integer...");
         System.out.println("Compiling this file without '@SuppressWarnings' would produce a warning:");
         System.out.println("\"synchronization on instance of a value-based class\"");
-        
+
         // This 'synchronized' block IS the demo.
         // It will compile, but the compiler warns against this practice.
         synchronized (myLock) {
