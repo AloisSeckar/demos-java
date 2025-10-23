64d4c571eaba458a41b1316e3463fd2bfcb8cb40
Nikesh-koila <kirthanbirwaz43@gmail.com>
2025-10-23 09:44:00 +0530

    feat(java16): Add demo for JEP 390 Value-Based Classes

--- src/main/java/org/javademos/init/Java16.java
+++ src/main/java/org/javademos/init/Java16.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import org.javademos.commons.IDemo;
 import org.javademos.java16.jep338.VectorAPIDemo;
 import org.javademos.java16.jep389.ForeignLinkerDemo;
+import org.javademos.java16.jep390.ValueBasedClassesDemo;
 import org.javademos.java16.jep393.ForeignMemoryAccessDemo;
 import org.javademos.java16.jep397.SealedClassesDemo;
 
@@ -22,6 +23,8 @@ public class Java16 {
         java16DemoPool.add(new VectorAPIDemo());
         // JEP 389
         java16DemoPool.add(new ForeignLinkerDemo());
+        //JEP 390
+        java16DemoPool.add(new ValueBasedClassesDemo());
         // JEP 393
         java16DemoPool.add(new ForeignMemoryAccessDemo());
         // JEP 397
