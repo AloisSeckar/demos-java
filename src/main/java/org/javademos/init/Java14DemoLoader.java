135d2dfc08cc2dbb745bc85a1b78fbeacf78fbab
Nayan Kaushik <102292267+NayanKaushik25@users.noreply.github.com>
2025-10-31 19:15:03 +0530

    feat(jdk14): Added demo for JEP 349 - JFR Event Streaming Demo (#347)
    * Added JFR Event Streaming Demo
    * Renamed the Demo File, added the demo to the json and created an instantiated the demo
    * Reordered the demo to maintain the ascending order in both the json file and the DemoLoader file
    * Update Java14DemoLoader.java

--- src/main/java/org/javademos/init/Java14DemoLoader.java
+++ src/main/java/org/javademos/init/Java14DemoLoader.java
@@ -7,6 +7,7 @@ import org.javademos.commons.IDemoLoader;
 
 import org.javademos.java14.jep305.InstanceofPatternMatchingPreview;
 import org.javademos.java14.jep343.PackagingToolIncubatorDemo;
+import org.javademos.java14.jep349.JFREventStreamingDemo;
 import org.javademos.java14.jep358.NullPointerDemo;
 import org.javademos.java14.jep359.RecordsPreviewDemo;
 import org.javademos.java14.jep361.SwitchExpressionsDemo;
@@ -21,11 +22,12 @@ import org.javademos.java14.jep370.ForeignMemoryAccessDemo;
  * Loads demos for Java 14.
  */
 public class Java14DemoLoader implements IDemoLoader {
-    
+
     @Override
     public void loadDemos(Map<Integer, IDemo> demos) {
         demos.put(305, new InstanceofPatternMatchingPreview());
         demos.put(343, new PackagingToolIncubatorDemo());
+        demos.put(349, new JFREventStreamingDemo());
         demos.put(358, new NullPointerDemo());
         demos.put(359, new RecordsPreviewDemo());
         demos.put(361, new SwitchExpressionsDemo());
