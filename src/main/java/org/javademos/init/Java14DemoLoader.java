f57d8f6086aa24c26a87f84ba10e2ae5ab312f2e
Sanjana Mahapatra <sanjanamahapatra46206@gmail.com>
2025-10-31 09:06:19 +0530

    JEP 343 – Packaging Tool (Incubator) changes

--- src/main/java/org/javademos/init/Java14DemoLoader.java
+++ src/main/java/org/javademos/init/Java14DemoLoader.java
@@ -6,6 +6,7 @@ import org.javademos.commons.IDemo;
 import org.javademos.commons.IDemoLoader;
 
 import org.javademos.java14.jep305.InstanceofPatternMatchingPreview;
+import org.javademos.java14.jep343.PackagingToolIncubatorDemo;
 import org.javademos.java14.jep358.NullPointerDemo;
 import org.javademos.java14.jep359.RecordsPreviewDemo;
 import org.javademos.java14.jep361.SwitchExpressionsDemo;
@@ -21,6 +22,7 @@ public class Java14DemoLoader implements IDemoLoader {
     @Override
     public void loadDemos(Map<Integer, IDemo> demos) {
         demos.put(305, new InstanceofPatternMatchingPreview());
+        demos.put(343, new PackagingToolIncubatorDemo());
         demos.put(358, new NullPointerDemo());
         demos.put(359, new RecordsPreviewDemo());
         demos.put(361, new SwitchExpressionsDemo());
