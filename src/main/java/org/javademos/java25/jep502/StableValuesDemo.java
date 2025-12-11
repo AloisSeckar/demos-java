7700b405ff20af7a5c18f3f68f714eeb7d163e51
Alois Sečkár <ellrohir@seznam.cz>
2025-12-11 18:47:54 +0100

    feat: JEP 502 - more obvious example

--- src/main/java/org/javademos/java25/jep502/StableValuesDemo.java
+++ src/main/java/org/javademos/java25/jep502/StableValuesDemo.java
@@ -13,25 +13,41 @@ import org.javademos.commons.IDemo;
 /// @author @CodeSakshamY / alois.seckar@gmail.com
 public class StableValuesDemo implements IDemo {
 
-    // stable value variable can be announced as "final", but not initialized immediately
-    private static final StableValue<int[]> STABLE_ARRAY = StableValue.of();
+    // this is the classic constant declaration in JDK <= 24
+    // it gets initialized immediately during class loading
+    // you will find the console log at the very begining of the program execution
+    private static final ExpensiveObject AS_CONSTANT = new ExpensiveObject(1);
 
-    // the initialization can happen ONCE at the first time when values are requested
+    // since JDK 25 we can init a "stable value" like this
+    // the variable is also treated as "private static final"
+    // but its initialization is deferred until the first time it is requested
+    private static final StableValue<ExpensiveObject> AS_STABLE_OBJECT = StableValue.of();
+
+    // the initialization of stable object will happen ONCE when values are requested the first time
     // "orElseSet" accepts a lambda supplier function to initialize the stable value with data
-    public static int[] getValues() {
-        return STABLE_ARRAY.orElseSet(() -> new int[]{10, 20, 30, 40, 50});
+    public static ExpensiveObject getExpensiveObject() {
+        return AS_STABLE_OBJECT.orElseSet(() -> new ExpensiveObject(2));
     }
 
     @Override
     public void demo() {
         info(502);
 
-        System.out.print("Stable array contents: ");
+        System.out.println("Stable Values demo started");
 
-        // here we ask for the stable values for the first time during runtime
-        for (int val : getValues()) {
-            System.out.print(val + " ");
-        }
+        // here we ask for the stable value for the first time during runtime
+        getExpensiveObject();
+
+        System.out.println(AS_CONSTANT);
+        System.out.println(AS_STABLE_OBJECT);
         System.out.println();
+    }
+}
+
+class ExpensiveObject {
+    public ExpensiveObject(int id) {
+        // imagine this class instance takes long to load 
+        // or consumes a lot of resources during that
+        System.out.println( "JEP 502 Demo - New ExpensiveObject " + id + " created!");
     }
 }
