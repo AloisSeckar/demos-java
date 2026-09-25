1675f2c81e50a4216a36acee83d2a9a1b890697f
Alois Sečkár <ellrohir@seznam.cz>
2026-09-25 22:28:55 +0200

    feat(jdk27): add demo for JEP 533 (#379)

--- src/main/java/org/javademos/java20/jep437/StructuredConcurrencyDemo.java
+++ src/main/java/org/javademos/java20/jep437/StructuredConcurrencyDemo.java
@@ -4,8 +4,8 @@ import org.javademos.commons.IDemo;
 
 /// Demo for JDK 20 feature JEP 437 - Structured Concurrency (Second Incubator).
 ///
-/// This was an incubator JEP. The feature's latest iteration is in JDK 25 by JEP 505.
-/// @see org.javademos.java25.jep505.StructuredConcurrencyDemo
+/// This was an incubator JEP. The feature's latest iteration is in JDK 27 by JEP 533.
+/// @see org.javademos.java27.jep533.StructuredConcurrencyDemo
 ///
 /// @author Adarsh80416
 public class StructuredConcurrencyDemo implements IDemo {
